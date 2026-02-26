d064977216f4d7c56f9a1c4d8eec48ef360357bb
akuahamda <hamdalamohammed80@gmail.com>
2026-02-26 07:00:47 -0800

    updated

--- Frontend/src/layouts/Marketplace.jsx
+++ Frontend/src/layouts/Marketplace.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/Footer/Footer";
 function Marketplace() {
   return (
     <>
-      <MarketPlaceNavbar />
+      <MarketplaceNavbar />
       <Outlet />
     </>
   );
